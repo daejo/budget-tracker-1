3fc9a27b3df4d8b292cabdfe1ed7573dae4ba46f
Marrione Nguyen <mtiamsic@gmail.com>
2020-12-05 23:23:00 -0800

    fixed service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,8 +8,8 @@ const FILES_TO_CACHE = [
     "./js/index.js",
     "./css/styles.css",
     "./icons/icon-72x72.png",
-    "./icons/icon-96x96.png", 
-    "./icons/icon-128x128.png", 
+    "./icons/icon-96x96.png",
+    "./icons/icon-128x128.png",
     "./icons/icon-144x144.png",
     "./icons/icon-152x152.png",
     "./icons/icon-192x192.png",
@@ -17,7 +17,7 @@ const FILES_TO_CACHE = [
     "./icons/icon-512x512.png",
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
             console.log('installing cache : ' + CACHE_NAME)
@@ -26,16 +26,16 @@ self.addEventListener('install', function(e) {
     )
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
     e.waitUntil(
-        caches.keys().then(function(keyList) {
-            let cacheKeeplist = keyList.filter(function(key) {
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
                 return key.indexOf(APP_PREFIX);
             });
             cacheKeeplist.push(CACHE_NAME);
 
             return Promise.all(
-                keyList.map(function(key, i) {
+                keyList.map(function (key, i) {
                     if (cacheKeeplist.indexOf(key) === -1) {
                         console.log('deleting cache : ' + keyList[i]);
                         return caches.delete(keyList[i]);
@@ -46,14 +46,22 @@ self.addEventListener('activate', function(e) {
     );
 });
 
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            }
-        })
-    )
+self.addEventListener("fetch", function (e) {
+    if (e.request.url.includes("/api/")) {
+        e.respondWith(
+            caches.open(DATA_CACHE_NAME).then(dataCache => {
+                return fetch(e.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            dataCache.put(e.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        return cache.match(e.request);
+                    });
+            }).catch(err => console.log(err))
+        );
+        return;
+    }
 })
