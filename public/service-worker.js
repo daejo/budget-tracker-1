64a40fdf2d16992920261625d3c070cda093488a
Marrione Nguyen <mtiamsic@gmail.com>
2020-12-05 23:48:04 -0800

    edit service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = 'data-cache-' + VERSION;
 
 const FILES_TO_CACHE = [
     "./index.html",
